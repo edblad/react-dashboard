f94502885880786c29614c649ff00b4bd532d642
Helena Edblad <helena.edblad@gmail.com>
2018-05-18 14:42:38 +0200

    half done currency component

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,15 @@ import React, { Component } from 'react';
 import Timer from './Timer';
 import Postit from './Postit';
 import Clock from './Clock';
-//import Currency from './Currency';
-import '../App.css';
 import Currency from './Currency';
+import '../App.css';
 
 class App extends Component {
   state = {
     postitText: localStorage.getItem('text'),
-    data: []
+    // data: [],
+    // EUR: '',
+    // SEK: ''
   }
 
   saveText = (event) => {
@@ -17,21 +18,31 @@ class App extends Component {
       localStorage.setItem('text', event.target.value);
   }
 
-  componentDidMount() {
-    this.getDataFromApi();
-  }
+  // componentDidMount() {
+  //   this.fetchCurrency();
+  // }
 
-  getDataFromApi = () => {
-    fetch('http://data.fixer.io/api/latest?access_key=fe21b7321aa53d1a4dd706d7fff65038')
-    .then(response => response.json())
-    .then(data => {
-        this.setState({ data: data });
-        return data;
-    })
-    .catch(error => {
-        console.log(error);
-    })
-  }
+  // fetchCurrency = () => {
+  //   fetch('http://data.fixer.io/api/latest?access_key=fe21b7321aa53d1a4dd706d7fff65038')
+  //   .then(response => response.json())
+  //   .then((data) => {
+  //       this.setState({ data });
+  //       this.calcCurrency();
+  //   })
+  //   .catch(error => {
+  //       console.log(error);
+  //   })
+  // }
+
+  // calcCurrency = () => {
+  //   this.setState({
+  //     EUR: this.state.data.base,
+  //     SEK: this.state.data.rates.SEK
+  //   });
+  //   console.log(this.state.EUR)
+  //   console.log(this.state.data.rates.SEK);
+  // }
+  
   
   render() {
     return (
@@ -39,7 +50,7 @@ class App extends Component {
         <Timer seconds={5} />
         <Postit value={this.state.postitText} saveText={this.saveText} />
         <Clock />
-       
+        <Currency currencyData={this.state.data} />
       </div>
     );
   }
--- src/components/Currency.js
+++ src/components/Currency.js
@@ -1,37 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function Currency(props) {
-    return props.currencyData.map((data) => {
+class Currency extends Component {
+    state = {
+        data: [],
+        EUR: '',
+        SEK: ''
+    }
+
+    componentDidMount() {
+        this.fetchCurrency();
+    }
+
+    fetchCurrency = () => {
+        fetch('http://data.fixer.io/api/latest?access_key=fe21b7321aa53d1a4dd706d7fff65038')
+        .then(response => response.json())
+        .then((data) => {
+            this.setState({ data });
+            this.calcCurrency();
+        })
+        .catch(error => {
+            console.log(error);
+        })
+    }
+
+    calcCurrency = () => {
+        this.setState({
+            EUR: this.state.data.base,
+            SEK: this.state.data.rates.SEK
+        });
+        console.log(this.state.EUR)
+        console.log(this.state.SEK);
+    }
+
+    render(){
         return (
-            <div> 
-                <p>{data.base}</p>
+            <div>
+                <button>Update currency</button>
+                <p>{this.state.SEK}</p>
             </div>
         );
-    });
+    }
 }
-
-// class Currency extends Component {
-    
-//     // componentDidMount() {
-//     //     this.getDataFromApi();
-//     // }
-    
-//     // getDataFromApi = () => {
-//     //     fetch('http://data.fixer.io/api/latest?access_key=fe21b7321aa53d1a4dd706d7fff65038')
-//     //     .then(response => response.json())
-//     //     .then(data => {
-//     //         this.setState({ data: data });  
-//     //     })
-//     //     .catch(error => {
-//     //         console.log(error);
-//     //     })
-//     // }
-
-//     render() {
-//         return (
-//             <div className="currency"></div>
-//         )
-//     }
-// }
 
 export default Currency;
