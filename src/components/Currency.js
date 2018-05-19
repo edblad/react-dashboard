d9bc81373706bcc616f03300a38ab2a573be51d0
Helena Edblad <helena.edblad@gmail.com>
2018-05-19 18:32:55 +0200

    changed the way state looks

--- src/components/Currency.js
+++ src/components/Currency.js
@@ -2,8 +2,11 @@ import React, { Component } from 'react';
 
 class Currency extends Component {
     state = {
-        EUR: '',
-        SEK: ''
+        date: '',
+        rates: {
+            EUR: '',
+            SEK: ''
+        }
     }
 
     componentDidMount() {
@@ -14,7 +17,7 @@ class Currency extends Component {
         fetch('http://data.fixer.io/api/latest?access_key=fe21b7321aa53d1a4dd706d7fff65038')
         .then(response => response.json())
         .then((data) => {
-            this.setState({ SEK: data.rates.SEK, EUR: data.rates.EUR  })
+            this.setState({ date: data.date, rates: { SEK: data.rates.SEK, EUR: data.rates.EUR } })
         })
         .catch(error => {
             console.log(error);
@@ -22,15 +25,16 @@ class Currency extends Component {
     }
 
     calcCurrency = () => {
-        let sum = this.state.EUR / this.state.SEK;
+        let sum = this.state.rates.EUR / this.state.rates.SEK;
         return sum.toFixed(3);
     }
 
     render(){
         return (
             <div>
-                <button>Update currency</button>
-                <p>{this.state.EUR} euro kostar {this.state.SEK} kr</p>
+                <button onClick={this.fetchCurrency}>Update currency</button>
+                <p>{ this.state.date }</p>
+                <p>{this.state.rates.EUR} euro kostar {this.state.rates.SEK} kr</p>
                 <p>1 kr kostar { this.calcCurrency() } euro</p>
             </div>
         );
